620d7843447eca2ee0969c9a8b5356fec439767b
Burhan uddin <burhan.uddin.baig1@gmail.com>
2022-01-07 17:52:25 +0500

    first commit

---
+++ polls/tests.py
@@ -0,0 +1,13 @@
+# import datetime
+
+# from django.test import TestCase
+# from django.utils import timezone
+
+# from .models import Question
+
+# # Create your tests here.
+
+# class QuestionModelTests(TestCase):
+#     def test_was_published_recently_with_future_question(self):
+#         time = timezone.now() + timeDelta(days = 30)
+
